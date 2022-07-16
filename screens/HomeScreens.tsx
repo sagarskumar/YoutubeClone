c274bb9fe9776cb72af5d29aca985640ab480c2d
sagarskumar <sagarskumar113@gmail.com>
2022-07-16 23:55:23 +0530

    sec1 commit

---
+++ screens/HomeScreens.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const HomeScreen =() => {
+
+    return(
+        <view>
+            <Text> Hello World</Text>
+        </view>
+    )
+}
+
+export default HomeScreen;
